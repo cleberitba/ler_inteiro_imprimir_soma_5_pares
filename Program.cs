a3b55f20a8c4c1d0b72434c7286f4764b7ef58d7
cleber Costa <66891353+cleberitba@users.noreply.github.com>
2022-02-19 20:56:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = int.Parse(Console.ReadLine());   
+            int number=0;                     
+
+            while
+            (!(x==0)){
+                    number=0;  
+                   for (int contador = 0; contador < 5; ){
+
+                       if (x % 2 == 0){
+                             number+=x;
+                             x++;
+                             contador++;
+                         }
+                         else{
+                             x++;
+                         }   
+                    }
+                    Console.WriteLine($"{number}");
+                    x = int.Parse(Console.ReadLine()); 
+                     
+            }
+
+             if(x==0)
+             {
+               Console.WriteLine(""); 
+             }
+                    
+        }
+    }
+ 
+
+      
+           
+          
+
+            
+        
+    
